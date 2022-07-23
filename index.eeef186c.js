73b86eba3477d48a222ef0d239c797ec978f3bb9
Osadchukua <105046159+Osadchukua@users.noreply.github.com>
2022-07-23 09:29:10 +0000

    Deploying to gh-pages from @ Osadchukua/team-project@2e0792e3959952f0b5bed9f92539e90d22028515 🚀

---
+++ index.eeef186c.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{loop:!0,autoplay:{delay:2e3,stopOnLastSlide:!0,disableOnInteraction:!1},pagination:{el:".swiper-pagination",clickable:!0,dynamicBullets:!0},mousewheel:{sensitivity:1,eventsTarget:".swiper"}});
+//# sourceMappingURL=index.eeef186c.js.map

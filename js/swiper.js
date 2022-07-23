31f90c70a03a877b16b7bbbb4b3cae9f405e89b9
nicefull <madnadika@gmail.com>
2022-07-23 12:20:48 +0300

    add customers

---
+++ js/swiper.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  // freeMode: true,
+  autoplay: {
+    delay: 2000,
+    stopOnLastSlide: true,
+    disableOnInteraction: false,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    // type: 'bulets',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  mousewheel: {
+    sensitivity: 1,
+    eventsTarget: '.swiper',
+  },
+});

2f97a2243ce90e29484e6c3ea28d0bb737515cf4
Osadchukua <105046159+Osadchukua@users.noreply.github.com>
2022-07-25 15:54:23 +0000

    Deploying to gh-pages from @ Osadchukua/team-project@bc0b94d1afd3a64cd9c9596c7cb6db531ec6a8e3 🚀

---
+++ index.1ae4aec2.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-mod-open]"),closeModalBtn:document.querySelector("[data-mod-close]"),modal:document.querySelector("[data-mod]"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.1ae4aec2.js.map

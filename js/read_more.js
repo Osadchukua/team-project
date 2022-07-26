9a8fe6058dbcc75ba2239b2bc91a163b253b7529
Oleg Andreiev <mr.olegandreev@gmail.com>
2022-07-27 02:54:50 +0300

    Виправляє помилки About

--- js/read_more.js
+++ js/read_more.js
@@ -13,3 +13,35 @@ function myFunction() {
     moreText.style.display = 'flex';
   }
 }
+
+(() => {
+  const refs = {
+    openMoreBtn: document.querySelector('[list-open]'),
+    // closeModalBtn: document.querySelector('[data-modal-close]'),
+    more: document.querySelector('[more-text]'),
+  };
+
+  refs.openMoreBtn.addEventListener('click', toggleModal);
+  // refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    // document.body.classList.toggle('modal-open');
+    refs.more.classList.toggle('text-shown');
+  }
+})();
+
+// function myFunction() {
+//   var dots = document.getElementById('dots');
+//   var moreText = document.getElementById('more');
+//   var btnText = document.getElementById('myBtn');
+
+//   if (dots.style.display === 'none') {
+//     dots.style.display = 'inline';
+//     btnText.innerHTML = 'Read more';
+//     moreText.style.display = 'none';
+//   } else {
+//     dots.style.display = 'none';
+//     btnText.innerHTML = 'Read less';
+//     moreText.style.display = 'flex';
+//   }
+// }
